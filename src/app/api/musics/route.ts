e3f768ab2aaf80fadb956e484f75a863ef388a7a
Andrelino clientes <andrelinoclientes@gmail.com>
2025-11-09 12:30:53 -0300

    ud

--- src/app/api/musics/route.ts
+++ src/app/api/musics/route.ts
@@ -26,6 +26,7 @@ export async function GET() {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
       },
+      // next: {revalidate: 3600}
     });
 
     if (!response.ok) {
